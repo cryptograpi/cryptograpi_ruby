10566d8584232771e013b831da346bab14fccb8a
Rodolfo Ruiz <rpruiz@me.com>
2021-08-21 13:08:42 -0500

    Making sure rspec runs correctly

--- demo.rb
+++ demo.rb
@@ -1,10 +1,13 @@
 require_relative 'lib/cryptograpi_ruby'
 
-CryptograpiRuby.config do |c|
-  c.api_token = '123'
-  c.project_id = '345'
+CryptograpiRuby.config do |cr|
+  cr.api_token = '92dx/a3u+l7Qoh7qF9R/86VO'
+  cr.project_id = 'ID5F9QCkYVSARzRHaybSpN8eCubw8k9tMnpuyJMuA/wJ'
 end
 
 
 puts CryptograpiRuby.api_token
 puts CryptograpiRuby.project_id
+
+
+
---
+++ spec/lib/cryptograpi_ruby_spec.rb
@@ -0,0 +1,7 @@
+describe CryptograpiRuby do
+  it 'is possible to provide config options' do
+    described_class.config do |c|
+      expect(c).to eq(described_class)
+    end
+  end
+end
